3faf81ffc8905400f08e4dcd39e53f3bd0dd2acb
laachachi <ayoub.laac@gmail.com>
2025-05-14 13:37:36 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import { HashRouter } from "react-router-dom"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
 
-<React.StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+)
